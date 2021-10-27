9a6e3738f8eadc2cfb3d62e1064365b8bbaf56bd
Everaldo Nascimento <everaldobass@gmail.com>
2021-10-27 15:52:07 -0300

    nlw crachá online

---
+++ nlw-heat/js/main.js
@@ -0,0 +1,40 @@
+const LinksSocialMedia = {
+    github: 'everaldobass',
+    youtube: '/channel/UCQoGlVBRbmfxA2gxE5owj4A',
+    facebook: '/everaldobass',
+    instagram: 'jakeliny.gracielly',
+    twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks(){
+
+    for (let li of socialLinks.children){
+
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+
+
+function getGitHubProfileInfos(){
+
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        userName.textContent = data.name 
+        userBio.textContent = data.bio 
+        userLink.href = data.html_url 
+        UserImage.src = data.avatar_url 
+        userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
+
+
